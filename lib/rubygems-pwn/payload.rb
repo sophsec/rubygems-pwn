920adf2c27a707596e5cf7c26ef052e2f1c74afc
Postmodern <postmodern.mod3@gmail.com>
2011-08-24 14:33:29 -0700

    Fixed Geeeentlemen.

--- lib/rubygems-pwn/payload.rb
+++ lib/rubygems-pwn/payload.rb
@@ -1,5 +1,3 @@
 say_puts = lambda { |m| puts("\a#{m}"); system('say',m) }
-say_puts["Geeeentle-men"]
-say_puts["All versions of Ruby-Gems are vulnerable"]
-say_puts["to persistent code injection"]
-say_puts["via the gem specs that are re-generated when you install a Gem"]
+say_puts["Geentlemen."]
+say_puts["All versions of Ruby-Gems are vulnerable to persistent code injection via the gem specs that are re-generated when you install a Gem."]
