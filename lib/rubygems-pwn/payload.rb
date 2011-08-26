e6beadb96e478ad6cba944b04b57a7808e9ad4d8
Postmodern <postmodern.mod3@gmail.com>
2011-08-25 17:29:17 -0700

    Added the URL for the patch.

--- lib/rubygems-pwn/payload.rb
+++ lib/rubygems-pwn/payload.rb
@@ -1,3 +1,6 @@
-say_puts = lambda { |m| puts("\a#{m}"); system('say',m) }
+beep = lambda { |m| puts "\a#{m}" }
+say_puts = lambda { |m| beep[m]; system('say',m) }
 say_puts["Geentlemen."]
 say_puts["All versions of Ruby-Gems are vulnerable to persistent code injection via the gem specs that are re-generated when you install a Gem."]
+
+beep["* https://github.com/rubygems/rubygems/pull/165"]
